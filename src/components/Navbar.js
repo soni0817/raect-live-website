84f8e643803c407b872bfa4b92f097f975b358a0
soni <sonivishwakarma2810@gmail.com>
2022-08-18 12:14:40 +0530

    react-files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
     return (
       <div className={styles.nav}>
         <div className={styles.leftDiv}>
-          <a href="/">
+          <a href="javascript:void(0)">
             <img
               alt=""
               src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
@@ -13,7 +13,7 @@ const Navbar = () => {
   
         <div className={styles.rightNav}>
           <div className={styles.user}>
-            <a href="/">
+            <a href="javascript:void(0)">
               <img
                 src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
                 alt=""
@@ -26,13 +26,13 @@ const Navbar = () => {
           <div className={styles.navLinks}>
             <ul>
               <li>
-                <a href="/">Log in</a>
+                <a href="javascript:void(0)">Log in</a>
               </li>
               <li>
-                <a href="/">Log out</a>
+                <a href="javascript:void(0)">Log out</a>
               </li>
               <li>
-                <a href="/">Register</a>
+                <a href="javascript:void(0)">Register</a>
               </li>
             </ul>
           </div>
